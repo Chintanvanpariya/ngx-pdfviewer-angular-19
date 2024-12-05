411fc1c60690f12db6507bcb0bf389a4af06209c
Chintanvanpariya <chintan5588@gmail.com>
2024-12-04 21:41:40 -0600

    server file update

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/heroku-app-chintan'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/heroku-app-chintan/browser/index.html'));
+
+// Serve static files from the 'dist' folder
+app.use(express.static(path.join(__dirname, 'dist/heroku-app-chintan/browser')));
+
+// For all routes, send the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/heroku-app-chintan/browser/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+const PORT = process.env.PORT || 3087;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+}); 
